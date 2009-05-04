2ef93d2063ebaf9790c2dd8ec84029e7e2b2fb96
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2009-05-04 10:05:11 +0000

    uniformisation des tests unitaires
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@143 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ unittests/runtests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import nose
+
+curdir = os.path.abspath(os.path.dirname(__file__))
+os.chdir(os.path.join(curdir, ".."))
+
+sys.path.insert(0, "lib")
+
+nose.main()
