d70f6a7949157539611a95c923858715cc31ea95
Gabriel DE PERTHUIS <gabriel.de-perthuis@c-s.fr>
2009-05-14 14:40:11 +0000

    Update plugin test since the plugin isn't autoloaded
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@183 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- unittests/test_plugins.py
+++ unittests/test_plugins.py
@@ -1,11 +1,14 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 from __future__ import absolute_import
 
+from vigilo.corr.pluginmanager import load_plugin
 from vigilo.corr.registry import get_registry
 
 def test_plugin_loading():
     # Loading of plugins listed in settings['PLUGINS_ENABLED']
     # is done at registry initialisation.
+    # Don't list this one for testing.
+    load_plugin('vigilo.corr.rules.test')
     assert get_registry().rules.lookup('TestRule').name == 'TestRule'
 
 
