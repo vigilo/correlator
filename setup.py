1a40185d3bde76a593f882440fa0879a27c8bcee
Gabriel DE PERTHUIS <gabriel.de-perthuis@c-s.fr>
2009-07-07 15:33:04 +0000

    Look at generating rpms with proper dependencies from eggs.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@289 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -1,6 +1,54 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
+from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 from distutils.core import setup
+from distutils.dist import Distribution as _Distribution
+
+class Distribution(_Distribution, object):
+    def __init__ (self, attrs=None):
+        if attrs is not None and 'install_requires' in attrs:
+            self.__install_requires = attrs.pop('install_requires')
+        super(Distribution, self).__init__(attrs)
+
+    def get_install_requires(self):
+        return self.__install_requires
+
+class bdist_rpm(_bdist_rpm, object):
+    """
+    Generates rpm dependency requirements for python dependencies.
+    """
+
+    """
+    XXX Maybe this should be moved out.
+    The mappings are distro-specific, we need m name mappings × n eggs.
+    Could fit into buildout, paver, etc, and possibly use pip.
+    """
+
+    user_options = _bdist_rpm.user_options + [
+            ('no-translate-req', None,
+                'do not translate distutils/setuptools dependencies into rpm dependencies'),
+            ]
+
+    boolean_options = _bdist_rpm.boolean_options + ['no-translate-req', ]
+
+    def initialize_options(self):
+        super(bdist_rpm, self).initialize_options()
+        self.no_translate_req = 0
+
+    def finalize_options(self):
+        super(bdist_rpm, self).finalize_options()
+        if self.no_translate_req:
+            return
+        # The distutils consensus
+        distutils_requires = self.distribution.get_requires()
+        # The setuptools consensus, unfortunately there's a difference
+        setuptools_requires = self.distribution.get_install_requires()
+        raw_requires = distutils_requires + setuptools_requires
+        if self.requires is None:
+            self.requires = []
+        for raw_req in raw_requires:
+            req = raw_req
+            self.requires.append(req)
 
 setup(name='vigilo-correlator',
         version='0.1',
@@ -47,5 +95,7 @@ setup(name='vigilo-correlator',
                 ],
             },
         package_dir={'': 'src'},
+        cmdclass={'bdist_rpm': bdist_rpm, },
+        distclass=Distribution,
         )
 
