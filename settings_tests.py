670da223da809d45c004294c8f045b360e098426
Vincent QUEMENER <vincent.quemener@c-s.fr>
2010-01-11 11:04:54 +0000

    Correction mineure de compute_hls_states, pour le cas où cette fonction est appelée alors que la règle hls_deps n'a pas été exécutée.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1610 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- settings_tests.py
+++ settings_tests.py
@@ -3,7 +3,17 @@ import logging
 
 MEMCACHE_SRV_COMMAND = '/usr/sbin/memcached'
 MEMCACHE_CONN_TYPE = 'inet'
-PLUGINS_ENABLED = ()
+PLUGINS_ENABLED = (
+#        'vigilo.corr.rules.high_level_state',
+#        'vigilo.corr.rules.same_target_same_time_interval',
+#        'vigilo.corr.rules.test',
+#        'vigilo.corr.rules.topo_deps',
+        'vigilo.corr.rules.hls_deps',
+        'vigilo.corr.rules.lls_deps',
+        'vigilo.corr.rules.update_attribute',
+        'vigilo.corr.rules.update_occurrences_count',
+        'vigilo.corr.rules.priority',
+        )
 LOGGING_PLUGINS = (
         'vigilo.pubsub.logging',
         'vigilo.corr.logging',
@@ -13,7 +23,7 @@ LOGGING_LEVELS = {}
 LOGGING_SYSLOG = False
 
 ## Set to None to connect using the CORR_JID host and SRV records.
-XMPP_SERVER_HOST = 'localhost'
+XMPP_SERVER_HOST = 'vigilo-dev'
 XMPP_PUBSUB_SERVICE = 'pubsub.localhost'
 
 # Nœuds XMPP de tests.
