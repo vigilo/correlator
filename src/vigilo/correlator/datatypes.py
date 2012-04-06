5cd190f9c1176dc4181eebd7d0db901497198780
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-04-06 11:42:48 +0200

    Ajout et mises à jour de copyrights, complète le [sha:3c99844]
    Change-Id: I47c53310f710006097927c666387207c1f4125aa
    Fixup: 3c99844c7630b66e3f267cc08d169a5c91b73572
    BuildBot: no-build

--- src/vigilo/correlator/datatypes.py
+++ src/vigilo/correlator/datatypes.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 """
 Définit un ensemble de types de données génériques.
 """
--- src/vigilo/correlator/test/__init__.py
+++ src/vigilo/correlator/test/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 """Tests unitaires du corrélateur."""
 
 # Aide pour le debugging des tests unitaires.
