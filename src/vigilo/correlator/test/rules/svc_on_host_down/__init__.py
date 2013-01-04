fcbfa51bcf0ce6d3a413e6f318991aa36de35462
Francois POIROTTE <francois.poirotte@c-s.fr>
2013-01-04 13:43:34 +0100

    Mise à jour des copyrights pour 2013.
    Change-Id: I8e82f08fb24cc7c5f3aa6e4b7fde7c65bc793f8e
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1085
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/correlator/test/rules/svc_on_host_down/__init__.py
+++ src/vigilo/correlator/test/rules/svc_on_host_down/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2012-2013 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 """
 Tests unitaires se rapportant à la règle de changement automatique
 de l'état des services en fonction de l'état de l'hôte.
