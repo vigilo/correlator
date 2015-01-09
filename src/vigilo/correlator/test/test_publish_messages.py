084826dc12835a9ed653e0f212659899853dc607
Francois POIROTTE <francois.poirotte@c-s.fr>
2015-01-09 18:28:24 +0100

    Mise à jour des copyrights pour 2015.
    Change-Id: I9284fbfa807afc51b301e47d93938da57372c338
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1556
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/correlator/test/test_publish_messages.py
+++ src/vigilo/correlator/test/test_publish_messages.py
@@ -1,6 +1,6 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 # pylint: disable-msg=C0111,W0212,R0904
-# Copyright (C) 2006-2014 CS-SI
+# Copyright (C) 2006-2015 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
