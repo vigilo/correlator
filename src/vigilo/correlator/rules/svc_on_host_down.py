942aec1418dc692c8939379ab6ff9f30cf4a6408
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-01-06 14:28:16 +0100

    Mise à jour des copyrights
    Change-Id: I0ac097e514832a05c112553fb5f54695066df170

--- src/vigilo/correlator/rules/svc_on_host_down.py
+++ src/vigilo/correlator/rules/svc_on_host_down.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set et ts=4 sw=4:
-# Copyright (C) 2006-2019 CS-SI
+# Copyright (C) 2006-2020 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
