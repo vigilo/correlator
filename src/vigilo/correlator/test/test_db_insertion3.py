a9c6656813f0668d1124f4b3bf90b163b7fd0717
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-06-30 17:08:03 +0200

    [#2026] Changement de dénomination sociale
    Change-Id: I40c85e76097c1b7cc3ded0a3c89a3a28e534b663

--- src/vigilo/correlator/test/test_db_insertion3.py
+++ src/vigilo/correlator/test/test_db_insertion3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable-msg=C0111,W0212,R0904
-# Copyright (C) 2006-2020 CS GROUP – France
+# Copyright (C) 2006-2020 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """Suite de tests des fonctions réalisant des insertions dans la BDD."""
