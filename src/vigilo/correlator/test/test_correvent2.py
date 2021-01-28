82a2f10ee87686777166b85f33baa46cef7879f4
Francois POIROTTE <francois.poirotte@c-s.fr>
2021-01-28 16:48:36 +0100

    Update the copyrights
    Change-Id: Ic166aa38b2ca7d09e745283371528e76bc473e05

--- src/vigilo/correlator/test/test_correvent2.py
+++ src/vigilo/correlator/test/test_correvent2.py
@@ -1,6 +1,6 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 # pylint: disable-msg=C0111,W0212,R0904
-# Copyright (C) 2006-2020 CS GROUP - France
+# Copyright (C) 2006-2021 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 from __future__ import print_function
